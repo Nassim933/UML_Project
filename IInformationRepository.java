4b659ae918f71851e3a7a2417f3f3fa63a7daba8
Nassim933 <79164583+Nassim933@users.noreply.github.com>
2022-12-12 19:29:59 +0200

    Delete IInformationRepository.java

--- IInformationRepository.java
+++
@@ -1,6 +0,0 @@
-import fr.efrei.domain.Information;
-import java.util.List;
-
-public interface IInformationRepository extends IRepository<Information, String>{
-    public List<Information> getAll();
-}
