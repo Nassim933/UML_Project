b319799813655e446fbcc4c23bd508065e41e43d
aimbinks <94440263+aimbinks@users.noreply.github.com>
2022-12-12 16:26:41 +0100

    Add files via upload

--- positionfactory.java
+++ positionfactory.java
@@ -1,22 +1,22 @@
 package fr.efrei.factory;
 
-import fr.efrei.domain.Type;
+import fr.efrei.domain.Position;
 import fr.efrei.util.Helper;
 
 public class PositionFactory {
-    public static Type creatType(String String, int ID_Position){
+    public static Position creatPosition(String Statue, int ID_Position){
         String id = Helper.generateId();
 
-        if ((Helper.isNullOrEmpty(String)) || Helper.isNullOrEmpty(ID_Position)){
+        if ((Helper.isNullOrEmpty(Statue)) || Helper.isNullOrEmpty(ID_Position)){
             return null;
         }
 
-        Type type = new Type.Builder()
+        Position Position = new Position.Builder()
                 .setId(id)
-                .setString(String)
+                .setStatue(Statue)
                 .setID_Position(ID_Position)
                 .build();
 
-        return type;
+        return Position;
     }
 }
